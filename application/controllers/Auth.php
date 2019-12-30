cdc74172897a0efe9057ca60ac507a0bc7b60c38
Mark Hester <marky360@live.ie>
2019-12-30 16:59:05 +0000

    Bcrypt encryption implementation

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * Class Auth
+ *
+ * @property Bcrypt $bcrypt
+ */
 class Auth extends MY_Controller
 {
     public function login()
@@ -10,5 +15,15 @@ class Auth extends MY_Controller
     public function logout()
     {
         //
+    }
+
+    /**
+     * @param $value
+     * @return mixed
+     *
+     * One way authentication system of password encryption.
+     */
+    public function encrypt($value) {
+        return $this->bcrypt->hash_password($value);
     }
 }
