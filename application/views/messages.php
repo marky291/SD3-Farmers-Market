d51e10c542ff3ec579f3e1dec98a396fadf56f2c
Mark Hester <marky360@live.ie>
2020-01-03 20:03:46 +0000

    Messages no longer minute em

--- application/views/messages.php
+++ application/views/messages.php
@@ -1,5 +1,5 @@
 <?php if ($this->session->flashdata('danger')) : ?>
-    <div class="alert alert-danger" role="alert">
+    <div class="alert alert-danger mb-0" role="alert">
         <div class="container">
             <?php echo $this->session->flashdata('danger'); ?>
         </div>
@@ -7,7 +7,7 @@
 <?php endif ?>
 
 <?php if (authenticated() && user()->is_admin) : ?>
-    <div class="alert alert-info" role="alert">
+    <div class="alert alert-info mb-0" role="alert">
         <div class="container">
             You are currently logged in with administrative privileges
         </div>
@@ -15,7 +15,7 @@
 <?php endif ?>
 
 <?php if ($this->session->flashdata('success')) : ?>
-    <div class="alert alert-success" role="alert">
+    <div class="alert alert-success mb-0" role="alert">
         <div class="container">
             <?php echo $this->session->flashdata('success'); ?>
         </div>
